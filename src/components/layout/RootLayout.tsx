9f1911f82a396ea5d6570a2a135f4c8f86e81e73
Marvelous <marvelous.udugbesi@icloud.com>
2026-02-07 18:35:11 +0000

    added test for login func

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,6 +1,17 @@
-import { Outlet } from 'react-router';
+import { Outlet, useNavigate } from 'react-router';
 import Footer from '../footer/Footer';
+import { useAuth } from '@/hooks/query/authQuery/useAuth';
+import { useEffect } from 'react';
 const RootLayout = () => {
+	const navigate = useNavigate();
+	const { isAuthenticated } = useAuth();
+
+	useEffect(() => {
+		if (!isAuthenticated) {
+			navigate('/');
+			return;
+		}
+	});
 	return (
 		<div className='w-screen relative'>
 			<Outlet />
